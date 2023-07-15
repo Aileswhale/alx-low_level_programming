eb4863a21473fb8818d2d68694f26e69c6405e31
aileswhale <ucheodilach@gmail.com>
2023-07-15 19:33:49 +0100

    modified

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+ * main- entry point
+ *
+ * return: always 0 (success)
+ */
+
+int main(void}
+{
+	int i;
+
+	for (i = 0; i < 10; 1++)
+{
+	putchar(i + '0');
+}
+	for (i = 0; i < 6; i++)
+{
+	putchar(1 + 'a');
+}
+	putchar('\n');
+
+	return (0);
+}
+
